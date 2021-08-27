5c5865aa21322171d07b4e2749b518b6bb749750
Andrey Znak <andreyznak@ester.co>
2021-08-27 13:02:58 +0300

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div className="App">
-      React app
-    </div>
+    <Header />
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar, Container, Button } from 'react-bootstrap';
+
+const Header = () => {
+  const user = false;
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="">Navbar</Navbar.Brand>
+          {user ? (
+            <Button variant="primary" className="button button_header">
+              Log In
+            </Button>
+          ) : (
+            <Button variant="primary" lassName="button button_header">
+              Log Out
+            </Button>
+          )}
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
